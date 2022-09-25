8f27a694170a515ac0c2681995a8eb71480a32f6
Aries Dimaranan <aasdimaranan@gmail.com>
2022-09-25 13:02:53 -0600

    replaced nvim-terminal with toggleterm

--- after/plugin/terminal.rc.lua
+++ after/plugin/terminal.rc.lua
@@ -1,5 +1,8 @@
-local status, terminal = pcall(require, 'nvim-terminal')
+local status, terminal = pcall(require, 'toggleterm')
 if (not status) then return end
 
-terminal.setup()
+terminal.setup({
+  open_mapping = [[<leader>;]]
+})
 
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
