c79018f87e49040866dcdaa0d0be0e1aebe2ff2c
Aries Dimaranan <aasdimaranan@gmail.com>
2023-01-10 17:26:08 -0700

    fix overwritten diffview maps

--- after/plugin/diffview.rc.lua
+++ after/plugin/diffview.rc.lua
@@ -1,14 +1,22 @@
 local status, diffview = pcall(require, "diffview")
+local actions = require('diffview.actions')
 if (not status) then return end
 
 diffview.setup({
-  use_icons = true,         -- Requires nvim-web-devicons
+  use_icons = true, -- Requires nvim-web-devicons
   view = {
     default = {
       layout = "diff2_horizontal"
     },
     merge_tool = {
       layout = "diff3_mixed"
+    }
+  },
+  keymaps = {
+    view = {
+      { "n", "<leader>ct", actions.conflict_choose("theirs"), { desc = "Choose the THEIRS version of a conflict" } },
+      { "n", "<leader>cb", actions.conflict_choose("base"), { desc = "Choose the BASE version of a conflict" } },
+      { "n", "<leader>ca", actions.conflict_choose("all"), { desc = "Choose all the versions of a conflict" } },
     }
   }
 })
