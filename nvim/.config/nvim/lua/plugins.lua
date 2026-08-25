714d14b6b1dd46264467476086b84522e37211b4
Aries Dimaranan <aasdimaranan@gmail.com>
2026-08-25 00:07:09 -0700

    null-ls -> conform. Setup Lua formatting

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,168 +1,199 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup({
+	"folke/tokyonight.nvim",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	},
+	"windwp/nvim-ts-autotag",
+	"nvim-lua/plenary.nvim",
+	"rmagatti/auto-session",
 
-require('lazy').setup(
-  {
-    'folke/tokyonight.nvim',
-    {
-      'nvim-treesitter/nvim-treesitter',
-      build = function()
-        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-        ts_update()
-      end,
-    },
-    "windwp/nvim-ts-autotag",
-    'nvim-lua/plenary.nvim',
-    'rmagatti/auto-session',
+	{
+		"m-demare/hlargs.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
 
-    {
-      'm-demare/hlargs.nvim',
-      dependencies = {
-        "nvim-treesitter/nvim-treesitter"
-      }
-    },
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	"nvim-telescope/telescope-file-browser.nvim",
 
-    -- Telescope
-    {
-      'nvim-telescope/telescope.nvim',
-      dependencies = { { 'nvim-lua/plenary.nvim' } }
-    },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    'nvim-telescope/telescope-file-browser.nvim',
+	"windwp/nvim-autopairs",
 
-    'windwp/nvim-autopairs',
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	"nvim-tree/nvim-web-devicons",
+	"nvim-lualine/lualine.nvim",
 
-    'nvim-tree/nvim-web-devicons',
-    'nvim-lualine/lualine.nvim',
+	-- LSP
+	"neovim/nvim-lspconfig",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+	},
+	-- CMP
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
 
-    -- LSP
-    'neovim/nvim-lspconfig',
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    {
-      "nvimtools/none-ls.nvim",
-      dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-      },
-    },
-    -- CMP
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
+	},
 
-    {
-    	"L3MON4D3/LuaSnip",
-    	version = "v2.*",
-    	build = "make install_jsregexp"
-    },
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = "neovim/nvim-lspconfig",
+	},
 
-     {
-       'SmiteshP/nvim-navic',
-       dependencies = 'neovim/nvim-lspconfig'
-     },
+	"ggandor/leap.nvim",
+	"kylechui/nvim-surround",
+	"nvim-tree/nvim-tree.lua",
 
-    'ggandor/leap.nvim',
-    'kylechui/nvim-surround',
-    'nvim-tree/nvim-tree.lua',
+	-- git stuff
+	{
+		"sindrets/diffview.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
+	"dinhhuy258/git.nvim",
+	"lewis6991/gitsigns.nvim",
 
-    -- git stuff
-    {
-      'sindrets/diffview.nvim',
-      dependencies = 'nvim-lua/plenary.nvim'
-    },
-    'dinhhuy258/git.nvim',
-    'lewis6991/gitsigns.nvim',
+	"stevearc/aerial.nvim",
+	"sitiom/nvim-numbertoggle",
+	"vim-test/vim-test",
+	"nacro90/numb.nvim",
 
-    'stevearc/aerial.nvim',
-    'sitiom/nvim-numbertoggle',
-    'vim-test/vim-test',
-    'nacro90/numb.nvim',
+	{ "akinsho/bufferline.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
 
-    { 'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons' },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	},
 
-    {
-      "ThePrimeagen/harpoon",
-      branch = "harpoon2",
-      dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" }
-    },
+	{
+		"rmagatti/goto-preview",
+		dependencies = { "rmagatti/logger.nvim" },
+		event = "BufEnter",
+		config = true,
+	},
 
-    {
-      "rmagatti/goto-preview",
-      dependencies = { "rmagatti/logger.nvim" },
-      event = "BufEnter",
-      config = true
-    },
+	{
+		"pwntester/octo.nvim",
+		cmd = "Octo",
+		opts = {
+			-- or "fzf-lua" or "snacks" or "default"
+			picker = "telescope",
+			-- bare Octo command opens picker of commands
+			enable_builtin = true,
+			use_local_fs = true,
+		},
+		keys = {
+			{
+				"<leader>op",
+				"<CMD>Octo pr list<CR>",
+				desc = "List GitHub PullRequests",
+			},
+			{
+				"<leader>on",
+				"<CMD>Octo notification list<CR>",
+				desc = "List GitHub Notifications",
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			-- OR "ibhagwan/fzf-lua",
+			-- OR "folke/snacks.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional if file_panel.icons is a function
+		},
+	},
 
-    {
-      "pwntester/octo.nvim",
-      cmd = "Octo",
-      opts = {
-        -- or "fzf-lua" or "snacks" or "default"
-        picker = "telescope",
-        -- bare Octo command opens picker of commands
-        enable_builtin = true,
-        use_local_fs = true
-      },
-      keys = {
-        {
-          "<leader>op",
-          "<CMD>Octo pr list<CR>",
-          desc = "List GitHub PullRequests",
-        },
-        {
-          "<leader>on",
-          "<CMD>Octo notification list<CR>",
-          desc = "List GitHub Notifications",
-        },
-      },
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
-        -- OR "ibhagwan/fzf-lua",
-        -- OR "folke/snacks.nvim",
-        "nvim-tree/nvim-web-devicons", -- optional if file_panel.icons is a function
-      },
-    },
+	{
+		"rmagatti/goto-preview",
+		dependencies = { "rmagatti/logger.nvim" },
+		event = "BufEnter",
+		config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+	},
 
-    {
-      "rmagatti/goto-preview",
-      dependencies = { "rmagatti/logger.nvim" },
-      event = "BufEnter",
-      config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
-    },
+	{
+		"mrcjkb/rustaceanvim",
 
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      },
-      keys = {
-        {
-          "<leader>?",
-          function()
-            require("which-key").show({ global = false })
-          end,
-          desc = "Buffer Local Keymaps (which-key)",
-        },
-      },
-    }
-  }
-)
+		version = "^9",
+		lazy = false,
+	},
+
+	{
+
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescript = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				graphql = { "prettierd" },
+				handlebars = { "prettierd" },
+			},
+			format_on_save = function(bufnr)
+				if vim.bo[bufnr].filetype == "ruby" then
+					return
+				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
+		},
+	},
+
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+})
--- nvim/.config/nvim/plugin/null-ls.lua
+++ nvim/.config/nvim/plugin/null-ls.lua
@@ -1,27 +1,14 @@
 local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- none-ls.nvim is unmaintained and still reads this table from its old
+-- location; Neovim 0.11's LSP client rewrite moved it under lsp.protocol,
+-- so without this shim any attach crashes in null-ls/client.lua.
+vim.lsp._request_name_to_capability = vim.lsp.protocol._request_name_to_capability
 
+-- Formatting is handled by conform.nvim; this is diagnostics-only.
 null_ls.setup({
   sources = {
     require("none-ls.diagnostics.eslint_d"),
-    null_ls.builtins.formatting.prettierd,
   },
-
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-      if ft == "ruby" then return end
-
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end
-  end,
 })
